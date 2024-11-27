dbd535ff1ee3b7bad485868478a4d298a18a5bcf
Georgiy-JO <wkone.mail@gmail.com>
2024-11-27 14:00:20 +0700

    whatever, dude

--- src/try.cpp
+++ src/try.cpp
@@ -1,33 +1,31 @@
 #include <iostream>
-#include <cmath>
-#include <iomanip>
 
-namespace sides {
-    double x, y, z;
-
-}
-
-void child_1(int n[]){
-    for (int i=0; i<5;i++){
-        std::cout<<n[i]<<std::endl; 
-
-    }
-}
-void child_2(int* n){
-    for (int i=0; i<5;i++){
-        std::cout<<n[i]<<std::endl; 
-
-    }
-}
 
 int main(){
     using namespace std;
-    int n[5]={2, 3, 4, 5, 10};
-
-    child_1(n);
-    child_2(n);
-
-
-
+    
+    int **matrix=nullptr;
+    int rows=0, columns=0;
+    cin>>rows;
+    cin>>columns;
+    matrix = new int*[rows];
+    for (int i=0;i<rows;i++){
+        matrix[i]=new int[columns];
+    }
+    for (int i=0;i<rows;i++){
+        for (int j=0;j<columns;j++){
+            cin>>matrix[i][j];
+        }
+    }
+    for (int i=0;i<rows;i++){
+        for (int j=0;j<columns;j++){
+            cout<<matrix[i][j]<<" ";
+        }
+    cout<<endl;
+    }
+    for (int i=0;i<rows;i++){
+        delete matrix[i];
+    }
+    delete matrix;
     return 0;
 }
