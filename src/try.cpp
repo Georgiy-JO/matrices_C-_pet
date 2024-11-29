f7d4873db9ba724fa3784f40f0a85396d426a17a
Georgiy-JO <wkone.mail@gmail.com>
2024-11-29 21:26:53 +0700

    first step

--- src/try.cpp
+++ src/try.cpp
@@ -1,31 +1,63 @@
 #include <iostream>
+#include <math.h>
 
-
+class Shape{
+    protected:
+        double volume=0;
+        double area=0;
+    public:
+    double getVolume(){
+        return volume;
+    }    
+    double getArea(){
+        return area;
+    }
+};
+class cube:public Shape{
+    public:
+        double side;
+    cube(double s){
+        side=s;
+        volume=pow(side,3);
+        area=6*pow(side,2);
+    }
+};
+class sphere:public Shape{
+    public:
+        double radius;
+    sphere(double r){
+        radius=r;
+        volume=(4.0/3.0)*M_PI*pow(radius,3);
+        area=4*M_PI*pow(radius,2);
+    }
+};
+class cylinder:public Shape{
+    public:
+        double radius;
+        double height;
+    cylinder(double r, double h){
+        radius=r;
+        height=h;
+        volume=M_PI*pow(radius,2)*height;
+        area=2*M_PI*radius*(radius+height);
+    }
+};
 int main(){
-    using namespace std;
+    using std::cout, std::endl;
+    cube kubic(5);
+    sphere sfera(5.5);
+    cylinder cylindr(3, 7);
+    cout<<"Kubik: "<<endl;
+    // cout<<"Area: "<<kubic.area<<endl;       //error
+    // cout<<"Volume: "<<kubic.volume<<endl;   //error
+    cout<<"Area: "<<kubic.getArea()<<endl;       //error
+    cout<<"Volume: "<<kubic.getVolume()<<endl;   //error
+    cout<<"Sfera: "<<endl;
+    cout<<"Area: "<<sfera.getArea()<<endl;
+    cout<<"Volume: "<<sfera.getVolume()<<endl;
+    cout<<"Cylindr: "<<endl;
+    cout<<"Area: "<<cylindr.getArea()<<endl;
+    cout<<"Volume: "<<cylindr.getVolume()<<endl;
     
-    int **matrix=nullptr;
-    int rows=0, columns=0;
-    cin>>rows;
-    cin>>columns;
-    matrix = new int*[rows];
-    for (int i=0;i<rows;i++){
-        matrix[i]=new int[columns];
-    }
-    for (int i=0;i<rows;i++){
-        for (int j=0;j<columns;j++){
-            cin>>matrix[i][j];
-        }
-    }
-    for (int i=0;i<rows;i++){
-        for (int j=0;j<columns;j++){
-            cout<<matrix[i][j]<<" ";
-        }
-    cout<<endl;
-    }
-    for (int i=0;i<rows;i++){
-        delete matrix[i];
-    }
-    delete matrix;
     return 0;
-}
+} 
